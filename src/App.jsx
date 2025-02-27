efbe176b5e1ab0b18699b49de48d7a4d13e63dd8
Volkan Çelebi <114937543+volkanchi@users.noreply.github.com>
2025-02-27 22:50:10 +0300

    Başlangıç
    Cem Hocanın repo kopyalandı

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,7 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return <h2 style={{ color: "blue" }}>Expense Item !</h2>;
+}
+
+export default ExpenseItem;
